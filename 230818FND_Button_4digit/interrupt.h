f17ae419876d8a26c910f61c7fe6e043daaf6089
triptocode <3580rio@gmail.com>
2023-08-21 18:33:52 +0900

    230818FND_Button_4digit - 숫자증가중에도 reset되게 코드수정

--- 230818FND_Button_4digit/interrupt.h
+++ 230818FND_Button_4digit/interrupt.h
@@ -1,12 +1,10 @@
-/*
+﻿/*
  * interrupt.h
  *
  * Created: 2023-08-18 오전 9:32:28
  *  Author: user
  */ 
-
-
-#ifndef INTERRUPT_H_
+#ifndef INTERRUPT_H_ // 여기서는 활용은 안하는 코드?
 #define INTERRUPT_H_
 
 #include <avr/io.h>
@@ -15,13 +13,14 @@
 #define FND_DIGIT_3 2
 #define FND_DIGIT_4 3
 
+// PG0 PG1 PG2 PG3 번 네자리가 FND숫자 네자리중 각 한자리씩 컨트롤
 #define FND_DIGIT_DDR DDRG	
 #define FND_DIGIT_PORT PORTG
+// PC0 PC1 PC2 PC3 PC4 PC5 PC6 총7개 자리가 FND숫자 한자리 화면에 0~9까지 데이터 컨트롤 
 #define FND_DATA_DDR DDRC
 #define FND_DATA_PORT PORTC
 
 //함수정의 
-
 void FND_Init();
 void FND_setFndDisplayData(uint16_t data);
 uint16_t FND_getFndDisplayData();
@@ -29,9 +28,5 @@ void FND_selectDigit(uint8_t digit);
 void fnd_showNumber(uint8_t fndNumber);
 void FND_off();
 void FND_ISR_Display();
-
-
-
-
 
 #endif /* INTERRUPT_H_ */
--- 230818FND_Button_4digit/main.c
+++ 230818FND_Button_4digit/main.c
@@ -12,82 +12,43 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-
-enum{STOP,RUN,RESET};
-	
-ISR(TIMER0_OVF_vect)
-{
-	FND_ISR_Display();
-	TCNT0=6;
-}
-
+enum{STOP,RUN,RESET}; // 0, 1, 2   // enum 의 초기값이 없으면 0부터~
+ISR(TIMER0_OVF_vect) { FND_ISR_Display(); TCNT0=6; }
 
 int main(void)
-{
-	
-	FND_Init();
+{	FND_Init();
 	TCCR0|=(1<<CS02)|(0<<CS01)|(0<<CS00);  //64분주   1/64 prescaler
 	TIMSK|=(1<<TOIE0);  //타이머 카운트 오버플로우 enable
 	TCNT0=6; //1ms만들기 위해
-
 	sei();
-	
 	uint16_t counter=0;
-	uint8_t counterState=STOP;
+	uint8_t counterState=STOP; // counterState = 0 
 	Button btnRunStop;
 	Button btnReset;
-	
-	Button_init(&btnRunStop,&BUTTON_DDR,&BUTTON_PIN,0);
-	Button_init(&btnReset,&BUTTON_DDR,&BUTTON_PIN,1);
-	
-	
-    /* Replace with your application code */
-    while (1) 
-    {
-		switch(counterState)
-		{
-			case STOP:
-			if(BUTTON_fetState(&btnRunStop)==ACT_PUSH)
-			{
-				counterState=RUN;
-
-			}
-			else if(BUTTON_fetState(&btnReset)==ACT_PUSH)
-			{
-
-				counterState=RESET;
-	
-			}
+	// Button_init(Button *button, volatile uint8_t *ddr, volatile uint8_t *pin, uint8_t pinNum)
+	Button_init(&btnRunStop, &BUTTON_DDR, &BUTTON_PIN,0); 
+	 // Button_init(Button &btnReset, volatile uint8_t &DDRA, volatile uint8_t &PINA, uint8_t 1)
+	Button_init(&btnReset, &BUTTON_DDR, &BUTTON_PIN,1);
+	 while (1)
+    {	switch(counterState) //	★ 1. 버튼 상황에 따른 로직의 case STOP, case RUN 
+		{	case STOP: //  case STOP 숫자멈춰있는상태 --> RUN(같은스위치 PUSH할때) 또는 RESET(옆에스위치 PUSH할때)
+				if(BUTTON_fetState(&btnRunStop)==ACT_PUSH) { counterState=RUN; }
+				else if(BUTTON_fetState(&btnReset)==ACT_PUSH) { counterState=RESET; }
+			break; 
+			case RUN: //case RUN 숫자증가하는 변화상태--> STOP(같은스위치 PUSH할때) 또는 RESET(옆에스위치 PUSH할때)
+				if(BUTTON_fetState(&btnRunStop)==ACT_PUSH) { counterState=STOP;}
+				else if(BUTTON_fetState(&btnReset)==ACT_PUSH) { counterState=RESET; }		
+		}		
+		switch(counterState) // ★ 2, FND 숫자디스플레이 상황에 따른 로직 case STOP,  case RUN , case RESET
+		{   case STOP: FND_setFndDisplayData(counter); // 숫자화면 멈춤
 			break; 
 			
-			case RUN:
-			if(BUTTON_fetState(&btnRunStop)==ACT_PUSH)
-			{
-				counterState=STOP;
-	
-			}
-	
-			
-		}
-		
-		switch(counterState)
-		{
-			case STOP:
-			FND_setFndDisplayData(counter);
-			break; 
-			
-			case RUN:
-			FND_setFndDisplayData(counter++);
-			
-			_delay_ms(100);
+			case RUN: FND_setFndDisplayData(counter++);  _delay_ms(100); // 숫자화면 증가중, 대기시간 100ms
 			break;
 			
-			case RESET:
-			counter=0;
-			FND_setFndDisplayData(counter);
-			counterState=STOP;
-			break;
+			case RESET:  counter=0;  FND_setFndDisplayData(counter);  counterState=STOP; // 숫자화면 0 으로 리셋, STOP상태로 리셋
+			break;	
 		}
-    }
-}
-
+    } // while end
+} // main end 
+ 
